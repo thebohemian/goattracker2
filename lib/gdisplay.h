8e9dfa4480a29c3c9201f3233bbe2d01f0ab23e3
Robert Schuster <robert.schuster.rs01@gmail.com>
2019-11-27 22:57:16 +0100

    first shot at making goatracker a library

---
+++ lib/gdisplay.h
@@ -0,0 +1,19 @@
+// TODO: Herausfinden, wo im Einsatz. Dort kann es Probleme geben
+
+#ifndef GDISPLAY_H
+#define GDISPLAY_H
+
+#define CNORMAL 8
+#define CMUTE 3
+#define CEDIT 10
+#define CPLAYING 12
+#define CCOMMAND 7
+#define CTITLE 15
+
+void printmainscreen(void);
+void displayupdate(void);
+void printstatus(void);
+void resettime(void);
+void incrementtime(void);
+
+#endif
