8694888736319e8db389c1f06800e68eb75a1fce
Robert Schuster <robert.schuster.rs01@gmail.com>
2019-12-01 01:18:24 +0100

    added binding functions and datatypes  added some more startup code

--- binding/kotlin/run.kts
+++ binding/kotlin/run.kts
@@ -1,15 +1,27 @@
 import com.github.thebohemian.libgoattracker.LibGoat2
+import com.github.thebohemian.libgoattracker.ParameterTableReference
 
 println("loading shared library ...")
 
 val lib = LibGoat2.load();
+
+lateinit var wv_l:ParameterTableReference
+lateinit var wv_r:ParameterTableReference
 
 println("loaded library: ${lib}. Now starting GoatTracker2 through native invocation.")
 
 // not providing any commandline arguments
 Thread {
 	lib.start_goattracker(0, emptyArray())
+
 }.apply {
 	isDaemon = true
 	start()
+}
+
+fun init() {
+	//lib.load_binary_song()
+
+	wv_l = lib.get_left_table(0)
+	wv_r = lib.get_right_table(0)
 }
--- binding/kotlin/src/LibGoat2.kt
+++ binding/kotlin/src/LibGoat2.kt
@@ -17,7 +17,27 @@ interface LibGoat2 : Library
 
 	fun stop_song()
 
-	fun get_instrument_pointer(instrumentNumber: Int): NativeInstrumentReference
+	fun load_binary_song(filename: String)
+
+	fun get_instrument(instrumentNumber: Int): NativeInstrumentReference
+
+	fun get_left_table(index: Int): ParameterTableReference
+
+	fun get_right_table(index: Int): ParameterTableReference
+
+	fun get_pattern(index: Int): PatternReference
+
+	fun get_pattern_lengths(): PatternLengthsReference
+
+	fun get_songorder(songIndex: Int, channelIndex: Int): SongOrderReference
+
+	fun get_song_lengths(): SongLengthsReference
+
+	fun get_song_name(): StringReference
+
+	fun get_author_name(): StringReference
+
+	fun get_copyright_name(): StringReference
 
 	/** Convenience method for more kotlinesque invocation */
 	fun start_goattracker(argc: Int, argv: List<String>) =
@@ -25,6 +45,22 @@ interface LibGoat2 : Library
 
 	companion object {
 		const val MAX_STR = 32
+
+		const val MAX_CHN = 3
+
+		const val MAX_TABLES = 4
+
+		const val MAX_TABLE_LEN = 255
+
+		const val MAX_PATT = 208
+
+		const val MAX_PATT_ROWS = 128
+
+		const val MAX_PATT_ROWS_DATA = MAX_PATT_ROWS * 4 + 4
+
+		const val MAX_SONGLEN = 254
+
+		const val MAX_SONGLEN_DATA = MAX_SONGLEN + 2
 
 		private val LIBRARY_NAME = "goat2"
 
@@ -55,6 +91,65 @@ class NativeInstrumentReference : Structure(), Structure.ByReference {
 		}
 
 	override fun getFieldOrder() = listOf("ad", "sr", "wavetableLine", "pulsetableLine", "filtertableLine", "speedtableLine", "vibdelay", "gatetimer", "firstwave", "_name")
+
+}
+
+class ParameterTableReference : Structure(), Structure.ByReference {
+
+	@JvmField var bytes = ByteArray(LibGoat2.MAX_TABLE_LEN)
+
+	fun setByte(index: Int, value: Int) { bytes[index] = value.toByte() }
+
+	override fun getFieldOrder() = listOf("bytes")
+
+}
+
+class StringReference : Structure(), Structure.ByReference {
+
+	@JvmField var _text = ByteArray(LibGoat2.MAX_STR)
+
+	override fun getFieldOrder() = listOf("_text")
+
+	var text: String 
+		get() = _text.toString(StandardCharsets.UTF_8)
+		set(text) {
+			text.toByteArray().let { it.copyInto(_text, 0, 0, Math.min(_text.size, it.size)) }
+		}
+}
+
+class PatternReference : Structure(), Structure.ByReference {
+
+	@JvmField var bytes = ByteArray(LibGoat2.MAX_PATT_ROWS_DATA)
+
+	fun setByte(index: Int, value: Int) { bytes[index] = value.toByte() }
+
+	override fun getFieldOrder() = listOf("bytes")
+
+}
+
+class PatternLengthsReference : Structure(), Structure.ByReference {
+
+	@JvmField var lengths = IntArray(LibGoat2.MAX_PATT)
+
+	override fun getFieldOrder() = listOf("lengths")
+
+}
+
+class SongOrderReference : Structure(), Structure.ByReference {
+
+	@JvmField var bytes = ByteArray(LibGoat2.MAX_SONGLEN_DATA)
+
+	fun setByte(index: Int, value: Int) { bytes[index] = value.toByte() }
+
+	override fun getFieldOrder() = listOf("bytes")
+
+}
+
+class SongLengthsReference : Structure(), Structure.ByReference {
+
+	@JvmField var lengths = IntArray(LibGoat2.MAX_CHN)
+
+	override fun getFieldOrder() = listOf("lengths")
 
 }
 
